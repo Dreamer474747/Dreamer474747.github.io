b969360b697622e7c836bab4eca1dec013600866
dreamer <123218770+Dreamer474747@users.noreply.github.com>
2023-05-09 10:52:21 +0430

    update

--- clock/script/script.js
+++ clock/script/script.js
@@ -3,27 +3,42 @@ const hourHand = $.querySelector('.hour-hand')
 const minuteHand = $.querySelector('.minute-hand')
 const secondHand = $.querySelector('.second-hand')
 const dateElem = $.querySelector('.date')
+let howMuchTimeLeftDiv = $.querySelector('.how-much-time-left')
+let howMuchTimeLeftDescription = $.querySelector('.how-much-time-left-description')
+let showTime = $.querySelector('.show-time')
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let time = new Date()
-let dayOfTheMonth = time.getDate()
-let dayOfTheWeek = time.getDay()
-let monthOfTheYear = time.getMonth()
-let year = time.getFullYear()
-let howMuchTimeLeft = $.querySelector('.how-much-time-left')
-let howMuchTimeLeftDescription = $.querySelector('.how-much-time-left-description')
+let time;
+let hour;
+let minute;
+let second;
+let milliSecond;
+let dayOfTheMonth;
+let dayOfTheWeek;
+let monthOfTheYear;
+let year;
 
 
 window.addEventListener('load', myTime)
 window.addEventListener('load', howMuchTimeLeftHandler)
+window.addEventListener('load', showTimeHandler)
+
+
+function theExactTime() {
+	time = new Date()
+	hour = time.getHours()
+	minute = time.getMinutes()
+	second = time.getSeconds()
+	milliSecond = time.getMilliseconds()
+	dayOfTheMonth = time.getDate()
+	dayOfTheWeek = time.getDay()
+	monthOfTheYear = time.getMonth()
+	year = time.getFullYear()
+}
 
 
 function myTime() {
-	let time = new Date()
-	let hour = time.getHours()
-	let minute = time.getMinutes()
-	let second = time.getSeconds()
-	let milliSecond = time.getMilliseconds()
+	theExactTime()
 
 	hourHand.style.transform = `rotate(${((hour + (minute / 60)) * 30)}deg)`
 	minuteHand.style.transform = `rotate(${((minute + (second / 60)) * 6)}deg)`
@@ -31,10 +46,8 @@ function myTime() {
 
 }
 
-setInterval(myTime, 1);
 
-
-
+theExactTime()
 dateElem.innerHTML = `${months[monthOfTheYear]} ${dayOfTheMonth + suffixOfTheDay()}, ${year} (${days[dayOfTheWeek]})`
 
 function suffixOfTheDay() {
@@ -50,19 +63,30 @@ function suffixOfTheDay() {
 }
 
 
-
 function howMuchTimeLeftHandler() {
-	let time = new Date()
-	let hour = time.getHours()
-	let minute = time.getMinutes()
-	let second = time.getSeconds()
+	theExactTime()
 
 	let timeLeft = (((hour * 60 * 60) + (minute * 60) + second) / 86400)
 	let timeLeftPercent = (timeLeft * 100).toFixed(1)
 
-	howMuchTimeLeft.style.width = `${100 - timeLeftPercent}%`
-	howMuchTimeLeftDescription.innerHTML = `${timeLeftPercent}% of today has passed. you still got ${(100 - timeLeftPercent).toFixed(1)}% remaining.`
+	howMuchTimeLeftDiv.style.width = `${100 - timeLeftPercent}%`
+
+	if (timeLeftPercent - Math.floor(timeLeftPercent) === 0) {
+		howMuchTimeLeftDescription.innerHTML = `${100 - timeLeftPercent}% of the day is still remaining.`
+	} else {		
+		howMuchTimeLeftDescription.innerHTML = `${(100 - timeLeftPercent).toFixed(1)}% of the day is still remaining.`
+	}
 }
 
-setInterval(howMuchTimeLeftHandler, 86000)
 
+function showTimeHandler() {
+	theExactTime()
+	
+	showTime.innerHTML = `${hour}:${minute}`
+}
+
+
+
+setInterval(myTime, 10);
+setInterval(howMuchTimeLeftHandler, 86000)
+setInterval(showTimeHandler, 1000)
