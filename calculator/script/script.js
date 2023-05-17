8ec51b7f2ad8965d407f0af4db703ae9b7c23d3a
dreamer <123218770+Dreamer474747@users.noreply.github.com>
2023-05-17 20:54:55 +0430

    update

---
+++ calculator/script/script.js
@@ -0,0 +1,147 @@
+const $ = document
+const outputQuestion = $.querySelector('.output__question')
+const outputAnswer = $.querySelector('.output__answer')
+const numberPads = $.querySelectorAll('.input__keypad--digit')
+const keypadAc = $.querySelector('.input__keypad--ac')
+const keypadC = $.querySelector('.input__keypad--c')
+const KeypadEqual = $.querySelector('.input__keypad--equal')
+const operations = $.querySelectorAll('.input__keypad--operation')
+const KeypadDot = $.querySelector('.input__keypad--dot')
+let operationIndex, result, firstNumber, secondNumber;
+const completeOperationRegex = /((\d|\.)+(\+|×|∧|÷|-){1}(\d|\.)+)/g;
+const halfOperationRegex = /(\d|\.)+(\+|×|∧|÷|-){1}/g;
+const dotRegex = /\.\d+(\+|-|×|∧|÷)\./g;
+
+
+window.addEventListener('load', () => {
+    outputQuestion.value = ''
+    outputAnswer.value = ''
+})
+
+
+
+
+numberPads.forEach(number => {
+    number.addEventListener('click', clickHandler)
+})
+
+
+function clickHandler(e) {
+    outputQuestion.value += e.target.firstChild.textContent
+}
+
+
+keypadC.addEventListener('click', () => {
+    outputQuestion.value = outputQuestion.value.slice(0, -1)
+})
+
+
+keypadAc.addEventListener('click', () => {
+    outputQuestion.value = ''
+    outputAnswer.value = ''
+})
+
+
+KeypadDot.addEventListener('click', () => {
+    if (outputQuestion.value.match(/^-(\d+)?\.\d+(\+|×|∧|÷|-)(\d+)$/g)) {
+        outputQuestion.value += '.'
+        return
+    } else if (outputQuestion.value === '.' || (outputQuestion.value).match(dotRegex) || (outputQuestion.value).match(/^\d+\.$/g) || (outputQuestion.value).match(/(\+|×|∧|÷|-)(\d+)?\.\d+/g) || (outputQuestion.value).match(/^(\d+)?\.\d+$/g) || (outputQuestion.value).match(/(\+|×|∧|÷|-)(\d+)\./g)) {
+        return
+    }
+    outputQuestion.value += '.'
+})
+
+
+
+operations.forEach(operation => {
+    operation.addEventListener('click', e => {
+        if (outputQuestion.value.match(completeOperationRegex)) {
+            calculator();
+        } else if (outputQuestion.value === '' || outputQuestion.value.match(halfOperationRegex) || outputQuestion.value.match(/(\d+)?\.\d+(\+|×|∧|÷|-)\./g) || outputQuestion.value === '.') {
+            return;
+        }
+
+        firstNumber = outputQuestion.value;
+        outputQuestion.value += e.target.firstChild.textContent;
+        operationIndex = outputQuestion.value.length - 1;
+    });
+});
+
+
+
+function calculator() {
+    if (outputQuestion.value.match(/^(\d+)?\.\d+(\+|×|∧|÷|-)\.$/g) || outputQuestion.value === '.' || outputQuestion.value.match(/^(\d+)$|^(\+|×|∧|÷|-)?(\d+)?\.\d+$/g)) {
+        outputAnswer.value = outputQuestion.value;
+        return;
+    }
+
+    let secondNumber = outputQuestion.value.substring(operationIndex + 1, outputQuestion.value.length);
+
+    if (secondNumber && firstNumber) {
+
+        switch (outputQuestion.value[operationIndex]) {
+            case '+':
+                result = sum(firstNumber, secondNumber);
+                break;
+            case '-':
+                result = subtraction(firstNumber, secondNumber);
+                break;
+            case '×':
+                result = multiplication(firstNumber, secondNumber);
+                break;
+            case '÷':
+                result = division(firstNumber, secondNumber);
+                break;
+            default:
+                result = pow(firstNumber, secondNumber);
+        }
+
+
+        if (Number.isInteger(result)) {
+            outputAnswer.value = outputQuestion.value = firstNumber = result;
+        } else {
+            outputAnswer.value = outputQuestion.value = firstNumber = result.toFixed(3);
+        }
+    } else {
+        if (outputQuestion.value.match(/^\d+\.$/g)) {
+            outputAnswer.value = outputQuestion.value.slice(0, -1);
+            outputQuestion.value = outputAnswer.value;
+        } else if (outputQuestion.value.match(/(\d+)?\.\d+/g)) {
+            outputAnswer.value = firstNumber;
+        } else {
+            outputAnswer.value = outputQuestion.value;
+        }
+    }
+	
+	
+}
+
+
+KeypadEqual.addEventListener('click', calculator)
+
+
+function sum(firstNum, secondNum) {
+    return +firstNum + +secondNum
+}
+
+
+function subtraction(firstNum, secondNum) {
+    return firstNum - secondNum
+}
+
+
+function multiplication(firstNum, secondNum) {
+    return firstNum * secondNum
+}
+
+
+function division(firstNum, secondNum) {
+    return firstNum / secondNum
+}
+
+
+function pow(firstNum, secondNum) {
+    return firstNum ** secondNum
+}
+
